f9017a9683b3171b83c134e5750c4c07b3ce63f5
eecamp <158230718+eecamp@users.noreply.github.com>
2024-03-06 19:40:44 -0800

    finalizing presentation stuff

--- analysis plots.R
+++ analysis plots.R
@@ -1,41 +1,15 @@
----
-title: "Analysis"
-output: 
-  html_document:
-    keep_md: yes
-date: "2024-03-03"
-message: false
----
-
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE) 
-```
-
-```{r}
-#Libraries
 library(tidyverse)
 library(ggplot2)
 library(dplyr)
 library(ggridges)
 library(vegan)
-```
-
-
-
-```{r}
 library(readr)
 bob_ross <- read_csv("data/bob_ross_paintings.csv")
 view(bob_ross)
-```
-
-```{r}
 abundant_colors <- bob_ross %>%
   group_by(season) %>%
   summarize(mean(num_colors))
 abundant_colors
-```
-
-```{r plot 1, dev = c("png", "jpg", "tiff", "ps")}
 bob_ross_longer <- bob_ross %>%
   pivot_longer(c("Black_Gesso", "Bright_Red", "Burnt_Umber", "Cadmium_Yellow", "Dark_Sienna", "Indian_Red", "Indian_Yellow", "Liquid_Black", "Liquid_Clear", "Midnight_Black", "Phthalo_Blue", "Phthalo_Green", "Prussian_Blue", "Sap_Green", "Titanium_White", "Van_Dyke_Brown", "Yellow_Ochre", "Alizarin_Crimson"), names_to = "Color", values_to = "Used") %>%
   filter(Used == 1) 
@@ -44,7 +18,7 @@ bob_ross_longer <- bob_ross %>%
 hex_codes <- data.frame(color = c("Alizarin_Crimson", "Black_Gesso", "Bright_Red", "Burnt_Umber", "Cadmium_Yellow", "Dark_Sienna", "Indian_Yellow", "Liquid_Black", "Liquid_Clear", "Midnight_Black", "Phthalo_Blue", "Phthalo_Green", "Prussian_Blue", "Sap_Green", "Titanium_White", "Van_Dyke_Brown", "Yellow_Ochre"), 
                         hex = c('#4E1500', '#000000', '#DB0000', '#8A3324', '#FFEC00', '#5F2E1F', '#FFB800', '#000000', '#FFFFFF', '#000000', '#0C0040', '#102E3C', '#021E44', '#0A3410', '#FFFFFF', '#221B15', '#C79B00'), 
                         hex_adjusted = c('#4E1500', '#828181', '#DB0000', '#8A3324', '#FFEC00', '#5F2E1F', '#FFB800', '#C9C8C8', '#D8F8FD', '#000000', '#3115AD', '#2281AD', '#064AA6', '#187525', '#FFFFFF', '#624428', '#C79B00')
-                        )
+)
 
 hex_assign = c("Alizarin_Crimson" = '#4E1500', "Black_Gesso" = '#828181', "Bright_Red" = '#DB0000', "Burnt_Umber" = '#8A3324', "Cadmium_Yellow" = '#FFEC00', "Dark_Sienna" = '#5F2E1F', "Indian_Yellow" = '#FFB800', "Liquid_Black" = '#C9C8C8', "Liquid_Clear" = '#D8F8FD', "Midnight_Black" = '#000000', "Phthalo_Blue" = '#3115AD', "Phthalo_Green" = '#2281AD', "Prussian_Blue" = '#064AA6', "Sap_Green" = '#187525', "Titanium_White" = '#FFFFFF', "Van_Dyke_Brown" = '#624428', "Yellow_Ochre" = '#C79B00')
 
@@ -55,19 +29,14 @@ color_density <- ggplot(data = bob_ross_longer, aes(x = season, y = Color, fill 
   theme(legend.position = "none")
 color_density
 ggsave(plot = color_density, filename = paste("color density 1"), device = "jpg")
-```
-```{r plot 2, dev = c("png", "jpg", "tiff", "ps")}
+
 color_density2 <- ggplot(data = bob_ross_longer, aes(x = season, fill = Color)) +
   geom_density(position= 'fill') +
   scale_fill_manual(values = hex_assign) +
   theme_classic() +
   theme()#legend.position = "none")
 color_density2
-```
 
-```{r}
-#species diversity: 2 indices, Simpson and Shannon
-#Data wrangling:
 tidybob <- bob_ross %>%
   select(c(painting_index, painting_title, season, Black_Gesso, Bright_Red, Burnt_Umber, Cadmium_Yellow, Dark_Sienna, Indian_Red, Indian_Yellow, Liquid_Black, Liquid_Clear, Midnight_Black, Phthalo_Blue, Phthalo_Green, Prussian_Blue, Sap_Green, Titanium_White, Van_Dyke_Brown, Yellow_Ochre, Alizarin_Crimson)) %>%
   drop_na() %>%
@@ -87,7 +56,7 @@ for (season_number in 1:31) {
   
   #Calculate simpson diversity
   simpsondiversity <- diversity(temp_data$number, index = "simpson", equalize.groups = FALSE,
-                      MARGIN = 1, base = exp(1))
+                                MARGIN = 1, base = exp(1))
 }
 
 #shannon diversity index
@@ -105,7 +74,7 @@ for (season_number in 1:31) {
   
   #Calculate shannon diversity
   shannondiversity <- diversity(temp_data2$number, index = "shannon", equalize.groups = FALSE,
-                      MARGIN = 1, base = exp(1))
+                                MARGIN = 1, base = exp(1))
 }
 
 seasonno <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31)
@@ -127,11 +96,3 @@ diversity_results %>%
   theme_minimal() +
   labs(x = "Season", y = "Shannon diversity index", title = "Species diversity of colors in Bob Ross paintings over time") +
   geom_line()
-
-
-```
-
-
-
-
-
